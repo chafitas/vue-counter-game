317dca94086462fea20ef15fa2abe52a188be843
Edgar Alvarez OKC <programador2@okcomputer.com.pe>
2025-07-03 17:57:29 -0500

    init GameCounter

---
+++ src/components/GameCounter.vue
@@ -0,0 +1,29 @@
+<script setup>
+console.log('componente de juego')
+const isActive = true
+</script>
+
+<template>
+    <button>menos</button>
+    <span class="counter" :class="{ 'counter--active': isActive }">contador: <span>0</span></span>
+    <button>más</button>
+</template>
+
+<style scoped lang="scss">
+.counter {
+    font-weight: bold;
+    font-size: 2.5rem;
+
+    &>span {
+        color: green;
+    }
+}
+
+.counter--active {
+    background-color: #ddd;
+}
+
+.counter--active button {
+    background-color: red;
+}
+</style>
