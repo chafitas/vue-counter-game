5fd14e325b6b4d62b5e299eade61bb615c3866f7
Edgar Alvarez OKC <programador2@okcomputer.com.pe>
2025-07-03 21:26:25 -0500

    props, ref

--- src/components/GameCounter.vue
+++ src/components/GameCounter.vue
@@ -1,29 +1,112 @@
 <script setup>
-console.log('componente de juego')
-const isActive = true
+
+import { ref } from 'vue'
+
+const counter = ref(0)
+const increment = () => {
+    counter.value++
+}
+const decrement = () => {
+    counter.value--
+}
+
+const { minNumber, maxNumber } = defineProps({
+    minNumber: {
+        type: Number,
+        required: true
+    },
+    maxNumber: {
+        type: Number,
+        required: true
+    },
+    example: {
+        type: String,
+        validator: (value) => value.includes('+', '-')
+    },
+    arrays: {
+        type: Array,
+        default: () => [1, 2, 3, 4]
+    },
+    objects: {
+        type: Object,
+        default: () => ({
+            name: "Edgar"
+        })
+    }
+})
+console.log(minNumber)
+console.log(maxNumber)
 </script>
 
 <template>
-    <button>menos</button>
-    <span class="counter" :class="{ 'counter--active': isActive }">contador: <span>0</span></span>
-    <button>más</button>
+    <div class="counter-game">
+        <span class="number">{{ counter }}</span>
+        <div class="button-group">
+            <button @click="decrement">-</button>
+            <button @click="increment">+</button>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
-.counter {
+.counter-game {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+}
+
+.number {
+    font-size: 5rem;
     font-weight: bold;
-    font-size: 2.5rem;
-
-    &>span {
-        color: green;
-    }
+    color: white;
+    opacity: 0.9;
+    transition: all 0.3s ease-in-out;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    text-align: center;
 }
 
-.counter--active {
-    background-color: #ddd;
+.number:hover {
+    transform: scale(1.1);
+    color: rgb(219, 234, 254);
 }
 
-.counter--active button {
-    background-color: red;
+.button-group {
+    display: flex;
+    gap: 1rem;
+}
+
+.clue {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    border-radius: 50%;
+    width: 2rem;
+    height: 2rem;
+    background-color: transparent;
+    color: white;
+    border: 2px solid white;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+/* Estilos botones */
+button {
+    width: 4rem;
+    height: 4rem;
+    font-size: 2rem;
+    border-radius: 1rem;
+    background-color: rgba(119, 170, 251, 0.914);
+    color: white;
+    border: none;
+    box-shadow:
+        0 4px 6px -1px rgba(0, 0, 0, 0.1),
+        0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    cursor: pointer;
+}
+
+button:hover:not(:disabled) {
+    background-color: rgba(26, 77, 188, 0.7);
+    border: 1px solid rgba(255, 255, 255, 0.5);
 }
 </style>
